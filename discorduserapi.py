b14e58208fd2b56214d7933a2094f72ee4916789
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-14 19:17:59 +0400

    Add or update file via git

--- discorduserapi.py
+++ discorduserapi.py
@@ -1,3 +1,4 @@
+from click import pass_context
 from selenium import webdriver
 from myby import by
 from selenium.webdriver.edge.options import Options
@@ -16,4 +17,9 @@ class DiscordUserAPI:
 
     def login(self, mail: str, password: str):
         self.driver.get("https://discord.com/login")
+        username_input = self.driver.find_element(by.id, 'uid_5')
+        password_input = self.driver.find_element(by.id, 'uid_7')
+
+        username_input.send_keys(mail)
+        password_input.send_keys(password)
         
