5233c4ccabb3f637b879281dc85ebb9b191567c8
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-14 19:20:09 +0400

    Add or update file via git

--- discorduserapi.py
+++ discorduserapi.py
@@ -1,7 +1,9 @@
-from click import pass_context
+
 from selenium import webdriver
 from myby import by
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class DiscordUserAPI:
     
@@ -14,9 +16,12 @@ class DiscordUserAPI:
             options.add_argument("start-maximized")
 
         self.driver = webdriver.Edge(options=options)
+        self.wait = WebDriverWait(self.driver, 10)
 
     def login(self, mail: str, password: str):
         self.driver.get("https://discord.com/login")
+        self.wait.until(EC.visibility_of_element_located((by.id, 'uid_5')))
+        
         username_input = self.driver.find_element(by.id, 'uid_5')
         password_input = self.driver.find_element(by.id, 'uid_7')
 
