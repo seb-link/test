677f5753f307cb824bae7fec60861bb9314e016e
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-14 19:21:29 +0400

    Add or update file via git

--- discorduserapi.py
+++ discorduserapi.py
@@ -21,10 +21,12 @@ class DiscordUserAPI:
     def login(self, mail: str, password: str):
         self.driver.get("https://discord.com/login")
         self.wait.until(EC.visibility_of_element_located((by.id, 'uid_5')))
-        
+
         username_input = self.driver.find_element(by.id, 'uid_5')
         password_input = self.driver.find_element(by.id, 'uid_7')
+        validate_button = self.driver.find_element(by.xpath, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/button[2]')
 
         username_input.send_keys(mail)
         password_input.send_keys(password)
+        validate_button.click()
         
