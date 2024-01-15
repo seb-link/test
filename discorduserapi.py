56eee5dc9570f3a4ebab46f92565c0111cc659f7
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-15 15:12:05 +0400

    Add or update file via git

--- discorduserapi.py
+++ discorduserapi.py
@@ -17,7 +17,7 @@ class DiscordUserAPI:
         else:
             options.add_argument("start-maximized")
 
-        self.driver = webdriver.Edge(options=options)
+        self.driver = webdriver.Edge(options=options)# pk pas chrome ????????????? #edgeonfire #chromeontop
         self.wait = WebDriverWait(self.driver, 10)
 
     def login(self, mail: str, password: str):
@@ -70,3 +70,4 @@ class DiscordUserAPI:
 
     def get_friends_online(self):
         pass
+        exit(0)
