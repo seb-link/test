5d69b657ae99a1e7ca99bfbc36e1eaab1362d40b
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-15 10:08:45 +0400

    Add or update file via git

--- discorduserapi.py
+++ discorduserapi.py
@@ -1,4 +1,5 @@
 
+import time
 from selenium import webdriver
 from myby import by
 from selenium.webdriver.edge.options import Options
@@ -35,7 +36,22 @@ class DiscordUserAPI:
             pass
 
     def send_dm(self, id_recv: str, msg_to_send: str):
-        self.driver.get(f"https://discord.com/channels/@me/{id_recv}")
+
+        self.wait.until(EC.visibility_of_element_located((by.xpath, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div/nav/div[2]/ul/li[1]/div/a/div/div[2]/div/div')))
+
+        time.sleep(2)
+        # Récupérer tous les éléments <a>
+        elements_a = self.driver.find_elements(by.tag_name, "a")
+
+        # Parcourir tous les éléments <a> et cliquer sur celui avec href égal à "/salut"
+        for a in elements_a:
+            if id_recv in a.get_attribute("href"):
+                a.click()
+                break
+            raise ValueError(
+                f"Impossible de trouver l'identifiant {id_recv}"
+            )
+        
         self.wait.until(EC.visibility_of_element_located((by.xpath, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div[2]/div[2]/div/div[1]/main/form/div/div/div/div[3]/div/div[2]')))
         msg_input = self.driver.find_element(by.xpath, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div[2]/div[2]/div/div[1]/main/form/div/div/div/div[3]/div/div[2]')
         msg_input.send_keys(msg_to_send)
